17f747d86eb13ed0040481d2cbfcb4b6b9193b7a
mac <mac@Users-MacBook-Pro.local>
2018-07-17 10:22:43 +0300

    Create invitation code xib.

---
+++ Kabab/InvitationCodeView.swift
@@ -0,0 +1,41 @@
+//
+//  InvitationCodeView.swift
+//  Kabab
+//
+//  Created by mac on 04/11/1439 AH.
+//  Copyright © 1439 mac. All rights reserved.
+//
+
+import UIKit
+
+class InvitationCodeView: UIView {
+    // MARK: ~ Properties
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var InvitationCodeTextField01: UITextField!
+    @IBOutlet weak var InvitationCodeTextField02: UITextField!
+    @IBOutlet weak var InvitationCodeTextField03: UITextField!
+    @IBOutlet weak var InvitationCodeTextField04: UITextField!
+    
+    // MARK: ~ Inits
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    // MARK: ~ Private Methods
+    private func commonInit() {
+        // Load xib.
+        Bundle.main.loadNibNamed("InvitationCodeView", owner: self, options: nil)
+        // Add the xib view to this view as subview.
+        addSubview(contentView)
+        // Make the xib view size the same as this view size.
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+
+}
