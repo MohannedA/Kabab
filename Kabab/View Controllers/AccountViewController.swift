e8b38cf459539a23c1708e5b2cf5dbb5dca727d8
mac <mac@Users-MacBook-Pro.local>
2018-07-18 09:19:09 +0300

    Design My Account view controller screen.

--- Kabab/View Controllers/AccountViewController.swift
+++ Kabab/View Controllers/AccountViewController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class AccountViewController: UIViewController {
 
+    @IBOutlet weak var fullNameView: UIView!
+    @IBOutlet weak var IDNumberView: UIView!
+    @IBOutlet weak var mobileNumberView: UIView!
+    @IBOutlet weak var emailView: UIView!
     
     // MARK: ~ Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set up views borders.
+        fullNameView.layoutIfNeeded()
+        fullNameView.layer.addBorder(edge: .top, color: UIColor.darkGray, thickness: 2)
+        IDNumberView.layoutIfNeeded()
+        IDNumberView.layer.addBorder(edge: .top, color: UIColor.darkGray, thickness: 2)
+        mobileNumberView.layoutIfNeeded()
+        mobileNumberView.layer.addBorder(edge: .top, color: UIColor.darkGray, thickness: 2)
+        emailView.layoutIfNeeded()
+        emailView.layer.addBorder(edge: .top, color: UIColor.darkGray, thickness: 2)
     }
 
 }
