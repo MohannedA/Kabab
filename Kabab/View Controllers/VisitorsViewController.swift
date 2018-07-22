f649b27449b2e2fc668e09d876a6903dafa281f9
mac <mac@Users-MacBook-Pro.local>
2018-07-22 11:33:43 +0300

    Implement visitors view main table view.

--- Kabab/View Controllers/VisitorsViewController.swift
+++ Kabab/View Controllers/VisitorsViewController.swift
@@ -1,58 +1,33 @@
 //
 //  ChecksTableViewViewController.swift
 //  Kabab
-//  Tutorial link: https://medium.com/swift-programming/swift-enums-and-uitableview-sections-1806b74b8138
 //  Created by mac on 21/10/1439 AH.
 //  Copyright © 1439 mac. All rights reserved.
 //
 
 import UIKit
 
-let MovieData = [
-    ["title": "Jason Bourne", "cast": "CheckedIn", "genre": "action"],
-    ["title": "Suicide Squad", "cast": "Margot Robbie, Jared Leto, Will Smith", "genre": "action"],
-    ["title": "Star Trek Beyond", "cast": "Chris Pine, Zachary Quinto, Zoe Saldana", "genre": "action"],
-    ["title": "Deadpool", "cast": "Ryan Reynolds, Morena Baccarin, Gina Carano", "genre": "action"],
-    ["title": "London Has Fallen", "cast": "Gerard Butler, Aaron Eckhart, Morgan Freeman, Angela Bassett", "genre": "action"],
-    ["title": "Ghostbusters", "cast": "Kate McKinnon, Leslie Jones, Melissa McCarthy, Kristen Wiig", "genre": "comedy"],
-    ["title": "Central Intelligence", "cast": "Dwayne Johnson, Kevin Hart", "genre": "comedy"],
-    ["title": "Bad Moms", "cast": "Mila Kunis, Kristen Bell, Kathryn Hahn, Christina Applegate", "genre": "comedy"],
-    ["title": "Keanu", "cast": "Jordan Peele, Keegan-Michael Key", "genre": "comedy"],
-    ["title": "Neighbors 2: Sorority Rising", "cast": "Seth Rogen, Rose Byrne", "genre": "comedy"],
-    ["title": "The Shallows", "cast": "Blake Lively", "genre": "drama"],
-    ["title": "The Finest Hours", "cast": "Chris Pine, Casey Affleck, Holliday Grainger", "genre": "drama"],
-    ["title": "10 Cloverfield Lane", "cast": "Mary Elizabeth Winstead, John Goodman, John Gallagher Jr.", "genre": "drama"],
-    ["title": "A Hologram for the King", "cast": "Tom Hanks, Sarita Choudhury", "genre": "drama"],
-    ["title": "Miracles from Heaven", "cast": "Jennifer Garner, Kylie Rogers, Martin Henderson", "genre": "drama"],
-]
-
 class VisitorsViewController: UIViewController {
     //MARK: ~ Properties
     @IBOutlet weak var tableView: UITableView!
     
+    //MARK: ~ Variables
+    var isCheckedIn: Bool = false
     var checkedInView = CheckedInView()
     var checkedOutView = CheckedOutView()
     
-    //MARK: ~ Variables
-    var isCheckedIn: Bool = false
+    // Define mock up data.
+    var visitorsDataItems: [[[String: String]]] = [[["Name": "Ghostbusters",  "Section": "Expected"],
+                                                    ["Name": "Keanu",  "Section": "Expected"]],[["Name": "Jason Bourne",  "Section": "CheckedIn"], ["Name": "Suicide Squad",  "Section": "CheckedIn"]], [["Name": "Star Trek Beyond",  "Section": "CheckedOut"], ["Name": "London Has Fallen",  "Section": "CheckedOut"]]]
+    // Define table sections.
+    var tableSections = [0, 1, 2]
     
-    // The magic enum to end our pain and suffering!
-    // For the most part the order of our cases do not matter.
-    // What is important is that our first case is set to 0, and that our last case is always `total`.
-    enum TableSection: Int {
-        case expected = 0, checkedIN, checkedOut, all, total
-    }
-    
-    // This is the size of our header sections that we will use later on.
+    // Define section height.
     let SectionHeaderHeight: CGFloat = 25
     
-    // Data variable to track our sorted data.
-    var data = [TableSection: [[String: String]]]()
-    
-    
+    // MARK: ~ Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        sortData()
         
         // Define checked in view.
         checkedInView = CheckedInView(frame: CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: 497))
@@ -68,25 +43,23 @@ class VisitorsViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        // Load visitors table data.
         tableView.reloadData()
-    }
-    
-    // When generating sorted table data we can easily use our TableSection to make look up simple and easy to read.
-    func sortData() {
-        data[.expected] = MovieData.filter({ $0["genre"] == "action" })
-        data[.checkedIN] = MovieData.filter({ $0["genre"] == "comedy" })
-        data[.checkedOut] = MovieData.filter({ $0["genre"] == "drama" })
-        data[.all] = MovieData.filter({ $0["genre"] == "indie" })
     }
     
     //MARK: Actions
     @IBAction func checkIn(_ sender: UIButton) {
         isCheckedIn = true
+        // For testing.
+        let section = 1
+        visitorsDataItems[section].append(["Name": "Test",  "Section": "CheckedIn"])
+        // Load visitors table data.
         tableView.reloadData()
     }
     
     @objc func onClickCheckedInDoneImage(sender: UITapGestureRecognizer!) {
         checkedInView.animateHideToBottom { (true) in
+            // Remove blur effect.
             self.view.viewWithTag(30)?.removeFromSuperview()
         }
     }
@@ -94,6 +67,7 @@ class VisitorsViewController: UIViewController {
     
     @objc func onClickCheckedOutDoneImage(sender: UITapGestureRecognizer!) {
         checkedOutView.animateHideToBottom{ (true) in
+            // Remove blur effect.
             self.view.viewWithTag(40)?.removeFromSuperview()
         }
     }
@@ -103,39 +77,26 @@ class VisitorsViewController: UIViewController {
 
 extension VisitorsViewController: UITableViewDataSource, UITableViewDelegate {
     
-    // As long as `total` is the last case in our TableSection enum,
-    // this method will always be dynamically correct no mater how many table sections we add or remove.
     func numberOfSections(in tableView: UITableView) -> Int {
-        return TableSection.total.rawValue
+        return tableSections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Using Swift's optional lookup we first check if there is a valid section of table.
-        // Then we check that for the section there is data that goes with.
-        if let tableSection = TableSection(rawValue: section), let movieData = data[tableSection] {
-            if isCheckedIn {
-                if section == 1 {
-                    return movieData.count
-                }
-                else {
-                    return 0
-                }
-            } else {
-                return movieData.count
+        if isCheckedIn { // If checked in button is clicked.
+            if section == 1 {
+                return visitorsDataItems[section].count
             }
+            else {
+                return 0
+            }
+        } else {
+            return visitorsDataItems[section].count
         }
-        return 0
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        // If we wanted to always show a section header regardless of whether or not there were rows in it,
-        // then uncomment this line below:
-        //return SectionHeaderHeight
-        
-        // First check if there is a valid section of table.
-        // Then we check that for the section there is more than 1 row.
-        if let tableSection = TableSection(rawValue: section), let movieData = data[tableSection], movieData.count > 0 {
-            if isCheckedIn {
+        if visitorsDataItems[section].count > 0 {
+            if isCheckedIn { // If checked in button is clicked.
                 if section == 1 {
                     return SectionHeaderHeight
                 }
@@ -145,31 +106,29 @@ extension VisitorsViewController: UITableViewDataSource, UITableViewDelegate {
             } else {
                 return SectionHeaderHeight
             }
-            
         }
         
         return 0
+        
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        // Add section properties.
         let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: SectionHeaderHeight))
         view.backgroundColor = UIColor.lightGray//UIColor(red: 253.0/255.0, green: 240.0/255.0, blue: 196.0/255.0, alpha: 1)
         let label = UILabel(frame: CGRect(x: 15, y: 0, width: tableView.bounds.width - 30, height: SectionHeaderHeight))
         label.font = UIFont.boldSystemFont(ofSize: 15)
         label.textColor = UIColor.white
-        if let tableSection = TableSection(rawValue: section) {
-            switch tableSection {
-            case .expected:
-                label.text = "Expected"
-            case .checkedIN:
-                label.text = "Checked In"
-            case .checkedOut:
-                label.text = "Checked Out"
-            case .all:
-                label.text = "All"
-            default:
-                label.text = ""
-            }
+        // Add section label.
+        switch tableSections[section] {
+        case 0:
+            label.text = "Expected"
+        case 1:
+            label.text = "Checked In"
+        case 2:
+            label.text = "Checked Out"
+        default:
+            label.text = ""
         }
         view.addSubview(label)
         return view
@@ -177,40 +136,33 @@ extension VisitorsViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        // Similar to above, first check if there is a valid section of table.
-        // Then we check that for the section there is a row.
-        if let tableSection = TableSection(rawValue: indexPath.section), let movie = data[tableSection]?[indexPath.row] {
-            if let titleLabel = cell.viewWithTag(10) as? UILabel {
-                titleLabel.text = movie["title"]
-            }
-            if let subtitleLabel = cell.viewWithTag(20) as? UILabel {
-                subtitleLabel.text = movie["cast"]
-            }
+        if let titleLabel = cell.viewWithTag(10) as? UILabel {
+            titleLabel.text = visitorsDataItems[indexPath.section][indexPath.row]["Name"]
+        }
+        if let subtitleLabel = cell.viewWithTag(20) as? UILabel {
+            subtitleLabel.text = visitorsDataItems[indexPath.section][indexPath.row]["Section"]
         }
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let currentCell = tableView.cellForRow(at: indexPath)
-        if let subtitle = currentCell?.viewWithTag(20) as? UILabel {
-            switch indexPath.section {
-            case 0: // Expected.
-                break
-            case 1: // Checked In.
-                view.addBlurEffect(30)
-                // // To make the checked out view over the blur effect.
-                view.bringSubview(toFront: checkedInView)
-                checkedInView.animateShowFromBottom(completion: nil)
-                break
-            case 2: // Checked Out.
-                view.addBlurEffect(40)
-                // To make the checked out view over the blur effect.
-                view.bringSubview(toFront: checkedOutView)
-                checkedOutView.animateShowFromBottom(completion: nil)
-                break
-            default:
-                break
-            }
+        switch indexPath.section {
+        case 0: // Item is clicked in expected section.
+            break
+        case 1: // Item is clicked in checked in section.
+            view.addBlurEffect(30)
+            // // To make the checked out view over the blur effect.
+            view.bringSubview(toFront: checkedInView)
+            checkedInView.animateShowFromBottom(completion: nil)
+            break
+        case 2: // Item is clicked in checked out section.
+            view.addBlurEffect(40)
+            // To make the checked out view over the blur effect.
+            view.bringSubview(toFront: checkedOutView)
+            checkedOutView.animateShowFromBottom(completion: nil)
+            break
+        default:
+            break
         }
     }
 
